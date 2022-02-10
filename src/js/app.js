04947dd8a143c998eff52afc5b0524cd234b1bba
Viktor590 <resident1081994@gmail.com>
2022-02-10 11:48:21 +0300

    added send form

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,17 @@ import burger from './modules/burger.js';
 import headerSlider from './modules/headerSlider.js'
 import slider from './modules/slider.js';
 import tabs from './modules/tabs.js';
-import tabSlide from './modules/tabSlide.js'
+// import tabSlide from './modules/tabSlide.js'
 import modal from './modules/modal.js'
+import form from './modules/form.js'
 
 
 
 
 window.addEventListener('DOMContentLoaded', () => {
+
+  let modalState = {};
+
   burger('body', '.header__burger', '.header__nav-burger', '.header__nav-close');
 
 
@@ -18,11 +22,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
   tabs('.rent__tabs-list', '.rent__item-visibility', '.rent__item-arrow', '.rent__item-novisibility');
 
-  tabSlide('.reviews__tabs-card', '.reviews__arrow-left',
-    '.reviews__arrow-right',
-    '.reviews__content-cards',
-    '.reviews__tabs');
+  // tabSlide('.reviews__tabs-card', '.reviews__arrow-left',
+  //   '.reviews__arrow-right',
+  //   '.reviews__content-cards',
+  //   '.reviews__tabs');
 
   modal('.maps__contact-btn', '.form-close', '.form__inner');
   modal('.cards-btn', '.form-close', '.form__inner');
+
+  form(modalState);
 })
---
+++ src/js/modules/form.js
@@ -0,0 +1,66 @@
+const form = (state) => {
+  const inputs = document.querySelectorAll('.form__input');
+  const forms = document.querySelector('.contact')
+
+  // const name = inputs[0].value;
+  // console.log(name);
+
+  // let message = {
+  //   success: `Спасибо ${inputs.value} , скоро свяжемся`,
+  //   failure: `Что-то пошло не так, попробуйте еще раз`
+  // }
+
+  const postData = async (url, data) => {
+    let res = await fetch(url, {
+      method: 'POST',
+      body: data
+    })
+    return await res.text();
+  }
+
+  const clearInputs = () => {
+    inputs.forEach(el => {
+      el.value = '';
+    })
+  }
+
+  forms.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let div = document.createElement('div');
+    div.classList.add('status-message');
+    div.style.cssText = `
+      font-size: 25px;
+      color: red;
+      padding: 25px 0;
+    `;
+
+    forms.appendChild(div)
+
+    const formData = new FormData(forms);
+
+    // console.log(formData);
+
+    for (let key in state) {
+      formData.append(key, state[key])
+    }
+
+    postData('server.php', formData)
+      .then(res => {
+        console.log(res);
+        div.textContent = `Спасибо ${inputs[0].value}, скоро свяжемся`;
+      })
+      .catch(error => {
+        console.log(error);
+        div.textContent = `Что-то пошло не так, попробуйте еще раз`;
+      })
+      .finally(() => {
+        clearInputs();
+        setTimeout(() => {
+          div.remove()
+        }, 3000)
+      })
+  })
+
+}
+export default form
